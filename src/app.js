b28358d551862e85a30bf2016a4acc42b8c5b658
MANOJ PATRA <patra.manoj0@gmail.com>
2017-10-01 14:08:12 +0530

    Update app to add logic for logout button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    // to track user log in status
+    state = { loggedIn: false };
+
     componentWillMount() {
+        // Initialize firebase app
         firebase.initializeApp({
             apiKey: 'AIzaSyC9zXQ5D1FCuhXAisvWZxRxMJCn5BhfA3k',
             authDomain: 'authentication-2c0dd.firebaseapp.com',
@@ -14,13 +18,35 @@ class App extends Component {
             storageBucket: 'authentication-2c0dd.appspot.com',
             messagingSenderId: '117512028972'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                // user is signed in
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    // to decide whether to display login form or logout button
+    renderContent() {
+        if (this.state.loggedIn) {
+            return (
+                <Button>
+                    Log out
+                </Button>
+            );
+        }
+
+        return <LoginForm />;
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                this.renderContent();
             </View>
         );
     }
