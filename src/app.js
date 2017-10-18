847b4a2204007bf4b496fc79a291c98f7fcf4546
MANOJ PATRA <patra.manoj0@gmail.com>
2017-10-19 05:05:19 +0530

    Add Library component to app

--- src/app.js
+++ src/app.js
@@ -1,73 +1,33 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import firebase from 'firebase';
-import { Header, Button, CardSection, Spinner } from './components/common';
-import LoginForm from './components/LoginForm';
+
+/**
+ * Provider is a JSX component used to magically
+ * make the store available to all container components
+ * in the application without passing it explicitly. 
+ */
+import { Provider } from 'react-redux';
+
+/**
+ * createStore returns a Redux store to read state, dispatch actions
+ * and subscribe to changes
+ */
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/libraryList';
 
 class App extends Component {
-    // to track user log in status
-    state = { loggedIn: null };
-
-    componentWillMount() {
-        // Initialize firebase app
-        firebase.initializeApp({
-            apiKey: 'AIzaSyC9zXQ5D1FCuhXAisvWZxRxMJCn5BhfA3k',
-            authDomain: 'authentication-2c0dd.firebaseapp.com',
-            databaseURL: 'https://authentication-2c0dd.firebaseio.com',
-            projectId: 'authentication-2c0dd',
-            storageBucket: 'authentication-2c0dd.appspot.com',
-            messagingSenderId: '117512028972'
-        });
-
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                // user is signed in
-                this.setState({ loggedIn: true });
-            } else {
-                this.setState({ loggedIn: false });
-            }
-        });
-    }
-
-    // to decide whether to display login form or logout button
-    renderContent() {
-        switch (this.state.loggedIn) {
-            case true:
-                return (
-                        <CardSection>
-                            <Button onPress={() => firebase.auth().signOut()}>
-                                Log Out
-                            </Button>
-                        </CardSection>
-                );
-            case false:
-                return <LoginForm />; 
-            default:
-                return (
-                    <View style={styles.spinnerStyle}>
-                        <Spinner size="large" />
-                    </View>
-                );
-        }
-    }
-
     render() {
         return (
-            <View style={{ flex: 1 }}>
-                <Header headerText="Authentication" />
-                {this.renderContent()}
-            </View>
+            <Provider store={createStore(reducers)}>
+                <View style={{ flex: 1 }}>
+                    <Header headerText="Tech Stack" />
+                    <LibraryList />
+                </View>
+            </Provider>
         );
     }
 }
-
-const styles = {
-    spinnerStyle: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'center'
-    }
-};
 
 export default App;
