78179fbe307ca31aa098b7dd7e969584bdad4021
MANOJ PATRA <patra.manoj0@gmail.com>
2017-10-19 05:12:17 +0530

    Add switch statement to handle actions

--- src/reducers/selectionReducer.js
+++ src/reducers/selectionReducer.js
@@ -1,4 +1,14 @@
-export default (state, action) => {
-    console.log(action);
-    return null;
+/**
+ * Initially state will be undefined;
+ * to avoid returning undefined from the reducer
+ * assign state to null
+ */
+
+export default (state = null, action) => {
+    switch (action.type) {
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+    }
 };
