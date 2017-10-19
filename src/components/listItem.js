3eade1077cf4f330e21114d27ccb000849daeb31
MANOJ PATRA <patra.manoj0@gmail.com>
2017-10-19 06:21:18 +0530

    Add logic for code optimization

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { 
+    Platform,
+    UIManager,
+    Text, 
+    TouchableWithoutFeedback, 
+    View,
+    LayoutAnimation 
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 // exporting many things at once
 import * as actions from '../actions';
 
 class ListItemComponent extends Component {
+
+    constructor() {
+        super();
+        if (Platform.OS === 'android') {
+            UIManager.setLayoutAnimationEnabledExperimental && 
+            UIManager.setLayoutAnimationEnabledExperimental(true);
+        }
+    }
+
+    // called whenever the component is 
+    // about to be rerendered to the device
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+
+    renderDescription() {
+        const { library, expanded } = this.props;
+        // Show description for only the selected item
+        // if expanded is true
+        if (expanded) {
+            return (
+                <CardSection>
+                    <Text style={{ flex:1 }}>{library.item.description}</Text>
+                </CardSection>
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
-        console.log(this.props);
         const { id, title } = this.props.library.item;
         return (
             <TouchableWithoutFeedback
@@ -20,6 +54,7 @@ class ListItemComponent extends Component {
                             {title}
                         </Text>
                     </CardSection>
+                    {this.renderDescription()}
                 </View>
             </TouchableWithoutFeedback>
         );
@@ -33,9 +68,14 @@ const styles = {
     }
 };
 
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.item.id;
+    return { expanded };
+};
+
 /**
  * 1. dispatches all actions to redux store
  * 2. passes these actions as props to the component
  */
-const ListItem = connect(null, actions)(ListItemComponent);
+const ListItem = connect(mapStateToProps, actions)(ListItemComponent);
 export default ListItem;
